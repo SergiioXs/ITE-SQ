edce3b9f1460299fda5e37fe777e72869586864e
Sergio Antonio Cruz Olivares <sylver560@gmail.com>
2016-05-15 05:49:31 -0700

    Add files via upload

---
+++ html/ThingsToDo.d
@@ -0,0 +1,39 @@
+// Cosas por hacer //
+
+HOME
+1.- Sistema: al ya aver introducido los datos mostrar en la pantalla inicial (home/inicio) estadisticos o something else
+			 advertir que se borraran todos los datos del sistema al crear uno nuevo
+			 crear Pdf del sistema completo dividido por Procesos
+2.- Proceso: QA, verificar que no tenga mas bugs, Remover procesos
+3.- Modulos: QA, agregar modulos, borrar Modulos
+6.- Ayuda: documentar el boton para mostrar ayuda acerca del sistema
+
+MODULO
+1.- QA
+2.- Realizar las acciones si/no de "Remover cliente"
+3.- Anexar boton de "Cerrar sesion"
+(?).- Si al mostrar al cliente que ya es su turno y hay n modulos iguales ¿A cual debera ir? SOLUCION (?): Un campo que contenga el nombre(2) de dicho modulo o que muestre el nombre de usuario "ModuloN" 
+
+REGISTRO:
+1.- Mostrar tabla de Pre->Registro que se usara con la app
+
+DISPLAY
+1.- hacer que solo se muestre una vez el usuario
+
+/*------------------------------------------------------------------------*/
+Ideas para la Aplicacion: (Maximo 3 dias)
+
+1.- Antes que nada pedira tu Identificador
+2.- Podras darte de alta en un proceso 
+3.- Te mostrara:
+			tu Turno
+			Tiempo estimado faltanto
+			Documento necesarios
+
+4.- No seras atendido hasta no confirmar en el modulo registro que ya estas en las instalaciones,
+	Al tener el tiempo de registro desde que te registraste al momento de llegar a las instalaciones
+	y que el Registro te active seras atendido rapidamente ya que tu tiempo es mayor al de los demas
+
+5.- API
+		/cliente/:id
+		
